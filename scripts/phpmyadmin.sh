e3f83d00a52cc71e54fb9eb18fa9c514246051f5
av75ro <apostol.victor@gmail.com>
2015-02-10 11:32:34 +0200

    Create phpmyadmin.sh

---
+++ scripts/phpmyadmin.sh
@@ -0,0 +1,6 @@
+echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/app-password-confirm password $1" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/admin-pass password $1" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/app-pass password $1" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none" | debconf-set-selections
+apt-get -y install phpmyadmin > /dev/null 2>&1
